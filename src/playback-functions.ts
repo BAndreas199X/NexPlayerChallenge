b080e4bc1818cce06d0f77278d4fb3d6b0f2ab48
BAndreas199X <bauerandreas1991@gmx.de>
2021-01-11 23:18:15 +0100

    Adding the first bit of the source code

---
+++ src/playback-functions.ts
@@ -0,0 +1,30 @@
+//Note that these function all out an animal is an interim only. 
+//I want to check whether the functions get called correctly before potentially putting effort into the implementation in vain 
+
+export function setContainer(divId:String) {
+    console.log("Oxen");
+}
+
+export function open(videoUrl:String) {
+    console.log("Tiger");
+}
+
+export function play(){
+    console.log("Rabbit");
+}
+
+export function pause(){
+    console.log("Snake");
+}
+
+export function seek(at:number) {
+    console.log("Horse");
+}
+
+export function stop() {
+    console.log("Goat");
+}
+
+export function destroy() {
+    console.log("Chimpanzee");
+}
