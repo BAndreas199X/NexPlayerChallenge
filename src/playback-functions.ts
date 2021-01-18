1531a13f9f4ed460a65b5bc8139c797c59a2a271
BAndreas199X <bauerandreas1991@gmx.de>
2021-01-18 02:54:19 +0100

    Completed functionality to initiate DashPlayer

--- src/playback-functions.ts
+++ src/playback-functions.ts
@@ -1,6 +1,15 @@
 //Note that these function all out an animal is an interim only. 
 //I want to check whether the functions get called correctly before potentially putting effort into the implementation in vain 
 
+//function to initiate the player, that has no media, yet
+//assumption: there is at least one videoElement on the page, the media will be played in the first
+export function createPlayer() {
+    var player:HTMLVideoElement = document.getElementsByTagName("video")[0];
+    player.autoplay = false;
+    player.id = "dashjs-player";
+    player.controls=true;
+}
+
 export function setContainer(divId:String) {
     console.log("Oxen "+divId);
 }
